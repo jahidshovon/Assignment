f3a21ba7cddc5a87ba73ceb92c40454c7185c9d1
Jahid Hasan <79994141+jahidshovon@users.noreply.github.com>
2021-09-11 05:02:33 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment
+ Its a javascript homework. Where shown something write into the table and after submit it show in front.
---
+++ custom.js
@@ -0,0 +1,7 @@
+
+
+function btnClicked() {
+
+    let para = document.getElementById('paragraph').innerHTML = document.myform.name.value;
+    console.log(para)
+}
